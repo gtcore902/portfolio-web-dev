f2022c78a4caca893f3e90ec6cc6a13f79b9e93b
gtcore902 <gaetan.tremois@gmail.com>
2024-03-27 12:40:08 +0100

    style: modify projects links

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+
 import './Projects.scss';
 
 const Projects = ({ title, projectUrl, imageUrl, description, className }) => {
@@ -11,7 +14,15 @@ const Projects = ({ title, projectUrl, imageUrl, description, className }) => {
         <div className="project__content__tag"></div>
         <h4>{title}</h4>
         <p>{description}</p>
-        <p>Voir le projet</p>
+        <p>
+          Voir le projet
+          <FontAwesomeIcon
+            style={{ marginLeft: '5px' }}
+            icon={faArrowUpRightFromSquare}
+            className="flash-color"
+            size="sm"
+          />
+        </p>
       </div>
     </Link>
   );
