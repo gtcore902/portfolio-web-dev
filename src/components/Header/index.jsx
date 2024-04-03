19219bdce95fa2e3ef820f2f131d7eb7738ce071
gtcore902 <gaetan.tremois@gmail.com>
2024-04-03 14:23:07 +0200

    style: adjust class style svg

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -42,7 +42,7 @@ const Header = ({ scrollToElement }) => {
       <div className="header__logo">
         {/* <FontAwesomeIcon icon={faCode} size="2xl" className="flash-color" /> */}
         <svg
-          class="filled"
+          className="filled"
           width="44px"
           height="35px"
           viewBox="0 0 24 24"
@@ -50,22 +50,22 @@ const Header = ({ scrollToElement }) => {
           xmlns="http://www.w3.org/2000/svg"
           stroke="#e94d35"
         >
-          <g pathLength="1" id="SVGRepo_bgCarrier" stroke-width="0" />
+          <g pathLength="1" id="SVGRepo_bgCarrier" strokeWidth="0" />
 
           <g
             pathLength="1"
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
 
           <g pathLength="1" id="SVGRepo_iconCarrier">
             <path
               d="M7 8L3 11.6923L7 16M17 8L21 11.6923L17 16M14 4L10 20"
               stroke="#e94d35"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </g>
         </svg>
